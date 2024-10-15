9af5835108d1a1605b6a0f21a957b6d70e1a3dc0
aka <akadev8@outlook.com>
2024-10-15 05:04:31 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'; // if needed
+
+const firebaseConfig = {
+    apiKey: "your-api-key",
+    authDomain: "your-auth-domain",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id",
+    measurementId: "your-measurement-id"
+};
+
+firebase.initializeApp(firebaseConfig);
